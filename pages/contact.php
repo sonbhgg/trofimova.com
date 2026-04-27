6788ce7304a09c5996664bd1e501f96c389ecdcc
Veronica <akim60452@gmail.com>
2026-04-27 16:00:18 +0300

    добавлен фавикон

--- pages/contact.php
+++ pages/contact.php
@@ -1,4 +1,14 @@
-<?php include '../includes/header.php'; ?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Контакты</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="icon" href="../logo.png" type="image/x-icon"> 
+</head>
+<body>
+    <?php include '../includes/header.php'; ?>
 <main>
     <h1>Контакты</h1>
     <form method="post">
@@ -9,4 +19,5 @@
     </form>
 </main>
 <?php include '../includes/footer.php'; ?>
-
+</body>
+</html>
